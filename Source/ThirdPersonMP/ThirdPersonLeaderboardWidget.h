6cdb6f2c48430ed5e717e5d13d83ab493dda4a03
Andrey Kharlanchev <meganite647@gmail.com>
2024-05-06 07:36:20 +0400

    Add HUD and stats component

---
+++ Source/ThirdPersonMP/ThirdPersonLeaderboardWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ThirdPersonLeaderboardWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THIRDPERSONMP_API UThirdPersonLeaderboardWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
