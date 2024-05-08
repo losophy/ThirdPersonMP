f74c1eb9be2c3521802f0d3356459ce8d459b782
Andrey Kharlanchev <meganite647@gmail.com>
2024-05-08 08:25:59 +0400

    Add game mode interface

---
+++ Source/ThirdPersonMP/ThirdPersonGameModeInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ThirdPersonGameModeInterface.h"
+
+// Add default functionality here for any IThirdPersonGameModeInterface functions that are not pure virtual.
