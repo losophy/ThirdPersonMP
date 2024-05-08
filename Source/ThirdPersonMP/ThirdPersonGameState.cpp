d19a5e1099cd15809857f48cd57183bd0a045271
Andrey Kharlanchev <meganite647@gmail.com>
2024-05-08 11:14:51 +0400

    Cleanup game state

--- Source/ThirdPersonMP/ThirdPersonGameState.cpp
+++ Source/ThirdPersonMP/ThirdPersonGameState.cpp
@@ -2,17 +2,3 @@
 
 
 #include "ThirdPersonGameState.h"
-
-AThirdPersonGameState::AThirdPersonGameState()
-{
-	// Set the default gamemode class to the bluprinted one
-	static ConstructorHelpers::FClassFinder<AGameModeBase> DefaultGameMode(
-		TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonMPGameMode.BP_ThirdPersonMPGameMode"));
-
-	if(DefaultGameMode.Succeeded())
-	{
-		GameModeClass = DefaultGameMode.Class;
-	}
-	
-	
-}
--- Source/ThirdPersonMP/ThirdPersonGameState.h
+++ Source/ThirdPersonMP/ThirdPersonGameState.h
@@ -7,25 +7,11 @@
 #include "ThirdPersonGameState.generated.h"
 
 /**
- * Extends the base game state to control leaderboard data,
- * such as scores, number of players and on.
+ * 
  */
 UCLASS()
 class THIRDPERSONMP_API AThirdPersonGameState : public AGameStateBase
 {
 	GENERATED_BODY()
-
-public:
-	
-	AThirdPersonGameState();
-	
-#pragma region Stats
-public:
-
-	// Retrieves the number of players in the world
-	UFUNCTION(BlueprintPure, Category="TPGameState")
-	FORCEINLINE int32 GetPlayerNum() const noexcept { return PlayerArray.Num(); }
-	
-#pragma endregion
 	
 };
