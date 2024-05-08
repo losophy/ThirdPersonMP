a3820c65cc96bddd24a22eb94a5ec4bd0468af27
Andrey Kharlanchev <meganite647@gmail.com>
2024-05-08 07:50:45 +0400

    Add player state and game state

---
+++ Source/ThirdPersonMP/ThirdPersonGameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "ThirdPersonGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THIRDPERSONMP_API AThirdPersonGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ThirdPersonMP/ThirdPersonPlayerState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "ThirdPersonPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THIRDPERSONMP_API AThirdPersonPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
